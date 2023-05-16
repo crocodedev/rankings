e93329ab54d18958601fb7ad94eb7cd4411a1c87
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-16 16:17:17 +0300

    add netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-netlify'
+
 import sveltePreprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   preprocess: sveltePreprocess({
     preserve: ['ld+json'],
   }),
   kit: {
     inlineStyleThreshold: Infinity,
-    adapter: adapter(),
+    adapter: adapter({
+      edge: false,
+      split: false,
+    }),
   },
 }
-
-export default config
