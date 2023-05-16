8accf9b84357d74a6ff6faea92269959af88f234
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-16 11:37:30 +0300

    rewrite include font (remote form url connect)

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -43,13 +43,4 @@
 
 <style global lang="scss">
   @import 'destyle.css/destyle.css';
-  @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap');
-
-  .hero {
-    &__title {
-      font-weight: bold;
-      font-size: 72px;
-      color: #07124a;
-    }
-  }
 </style>
--- src/routes/[slug=hubpage]/+page.svelte
+++ src/routes/[slug=hubpage]/+page.svelte
@@ -149,15 +149,6 @@
 
 <style global lang="scss">
   @import 'destyle.css/destyle.css';
-  @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap');
-
-  .hero {
-    &__title {
-      font-weight: bold;
-      font-size: 72px;
-      color: #07124a;
-    }
-  }
 
   .card__image-icon {
     position: absolute;
