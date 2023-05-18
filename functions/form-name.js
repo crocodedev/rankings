87773ce8a6125a5059aa549a317e5f0194a6aaba
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-18 14:17:29 +0300

    fix form

--- functions/form-name.js
+++ functions/form-name.js
@@ -1,6 +1,13 @@
 exports.handler = function (event, context, callback) {
+  if (event.httpMethod !== 'POST') {
+    return callback(null, {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Invalid HTTP method' }),
+    })
+  }
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Форма успешно отправлена' }),
+    body: JSON.stringify({ message: 'Form submitted successfully' }),
   })
 }
