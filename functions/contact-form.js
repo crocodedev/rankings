6db83a841f0b0c3cb787ea8b84590eb8fa13f60a
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-26 10:10:11 +0300

    try make form

--- functions/contact-form.js
+++ functions/contact-form.js
@@ -10,11 +10,56 @@ exports.handler = function (event, context, callback) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'text/html', // Set the content type to HTML
   }
+
+  const response = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Form Submission</title>
+        <style>
+          /* Add your custom CSS styles here */
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            padding: 20px;
+          }
+
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #ffffff;
+            border: 1px solid #cccccc;
+            border-radius: 5px;
+            padding: 40px;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+          }
+
+          h1 {
+            font-size: 24px;
+            color: #333333;
+            margin-top: 0;
+          }
+
+          p {
+            font-size: 16px;
+            color: #666666;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Form Submitted Successfully</h1>
+          <p>Thank you for submitting the form. We have received your information.</p>
+        </div>
+      </body>
+    </html>
+  `
 
   callback(null, {
     statusCode: 200,
-    headers, // Добавляем заголовки в ответ
-    body: JSON.stringify({ message: 'Form submitted successfully' }),
+    headers,
+    body: response,
   })
 }
