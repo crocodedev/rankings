cda2a6ee773ef79b8e259d89d151fe3f7d6373bf
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-29 15:18:40 +0300

    eee

--- src/lib/sections/Form.svelte
+++ src/lib/sections/Form.svelte
@@ -204,7 +204,7 @@
           <input type="hidden" name="form-name" value="pizzaOrder" />
           <label>
             What order did the pizza give to the pineapple?
-            <input name="order" type="text" onChange={handleChange} />
+            <input name="order" type="text" />
           </label>
           <input type="submit" />
         </form>
