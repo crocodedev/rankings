bf93359720ead580764982741c4bea461d6279f0
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2023-05-31 17:08:04 +0300

    added prerender

--- src/lib/sections/Form.svelte
+++ src/lib/sections/Form.svelte
@@ -3,16 +3,21 @@
 </script>
 
 <script>
+  import Container from '$lib/components/Container.svelte'
+  import { beforeUpdate, onMount } from 'svelte'
+
   export let data = {}
 
-  import Container from '$lib/components/Container.svelte'
-  import { beforeUpdate } from 'svelte'
-
+  let isFormVisible = false
   let page = ''
 
   beforeUpdate(() => {
     let pathname = window.location.pathname
     page = pathname.substring(pathname.lastIndexOf('/') + 1)
+  })
+
+  onMount(() => {
+    isFormVisible = true
   })
 
   let isSubmitting = false
@@ -169,60 +174,64 @@
             {/each}
           </nav>
         </div>
-        <div class="contact-form__inner">
-          <form
-            method="POST"
-            class="contact-form__form"
-            name="contact-form-form-netlify"
-            id="contact-second"
-            action="/"
-            data-netlify="true"
-            on:submit|preventDefault={handleSubmit}
-          >
-            <input type="hidden" name="form-name" value="contact-form-form-netlify" />
-            <div class="contact-form__form-wrapper">
-              <div class="contact-form__input-wrapper">
-                <label for="name" class="contact-form__label">Your name</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="{data.inputName}*"
-                  class="contact-form__input"
-                  required
-                />
+
+        {#if isFormVisible}
+          <div class="contact-form__inner">
+            <form
+              method="POST"
+              class="contact-form__form"
+              name="contact-form-form-netlify"
+              id="contact-second"
+              action="/"
+              data-netlify="true"
+              on:submit|preventDefault={handleSubmit}
+            >
+              <input type="hidden" name="form-name" value="contact-form-form-netlify" />
+              <div class="contact-form__form-wrapper">
+                <div class="contact-form__input-wrapper">
+                  <label for="name" class="contact-form__label">Your name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="{data.inputName}*"
+                    class="contact-form__input"
+                    required
+                  />
+                </div>
+                <div class="contact-form__input-wrapper">
+                  <label for="email" class="contact-form__label">Your email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="{data.inputEmail}*"
+                    class="contact-form__input"
+                    required
+                  />
+                </div>
               </div>
-              <div class="contact-form__input-wrapper">
-                <label for="email" class="contact-form__label">Your email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="{data.inputEmail}*"
-                  class="contact-form__input"
-                  required
-                />
+              <div class="contact-form__form-wrapper contact-form__form-wrapper--column">
+                <div class="contact-form__input-wrapper">
+                  <label for="message" class="contact-form__label">Tell us about your project</label
+                  >
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows="5"
+                    placeholder={data.inputMessage}
+                    class="contact-form__input"
+                  />
+                </div>
+                <div class="contact-form__button-wrapper">
+                  <input type="submit" value={data.buttonText} class="btn" />
+                  <p class="contact-form__form-policy">{data.policyText}</p>
+                </div>
+                <span class="contact-form__succes" />
               </div>
-            </div>
-            <div class="contact-form__form-wrapper contact-form__form-wrapper--column">
-              <div class="contact-form__input-wrapper">
-                <label for="message" class="contact-form__label">Tell us about your project</label>
-                <textarea
-                  name="message"
-                  id="message"
-                  rows="5"
-                  placeholder={data.inputMessage}
-                  class="contact-form__input"
-                />
-              </div>
-              <div class="contact-form__button-wrapper">
-                <input type="submit" value={data.buttonText} class="btn" />
-                <p class="contact-form__form-policy">{data.policyText}</p>
-              </div>
-              <span class="contact-form__succes" />
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
+        {/if}
       </div>
     </Container>
   </section>
